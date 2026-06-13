595b01f8748c790022d95fca1e58f1197866f7fe
Isabele Correia <228397914+devcorreiaisabele@users.noreply.github.com>
2026-06-12 23:19:16 -0300

    Add method to find PlanoReceita by Plano ID

--- src/main/java/com/example/backend3nareplica/repository/PlanoReceitaRepository.java
+++ src/main/java/com/example/backend3nareplica/repository/PlanoReceitaRepository.java
@@ -1,7 +1,8 @@
 package com.example.backend3nareplica.repository;
-
 import com.example.backend3nareplica.entity.PlanoReceita;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PlanoReceitaRepository extends JpaRepository<PlanoReceita, Long> {
+    List<PlanoReceita> findByPlanoIdPlano(Long idPlano);
 }
