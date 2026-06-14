44abe75f985faca446ff8cb1d4cf6103a5e60fc5
Isabele Correia <228397914+devcorreiaisabele@users.noreply.github.com>
2026-06-14 12:39:18 -0300

    Create GrupoRepository interface for Grupo entity

---
+++ src/main/java/com/example/backend3nareplica/repository/GrupoRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend3nareplica.repository;
+
+import com.example.backend3nareplica.entity.Grupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrupoRepository extends JpaRepository<Grupo, Long> {
+}
